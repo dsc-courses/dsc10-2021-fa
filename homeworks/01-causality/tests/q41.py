bf9cb7e7402f39867ad54febcafebcd7c19fb452
xd00099 <dxiang@ucsd.edu>
2021-09-25 17:11:14 -0700

    added hw1

---
+++ homeworks/01-causality/tests/q41.py
@@ -0,0 +1,1 @@
+test = {'name': 'q41', 'points': 1, 'suites': [{'cases': [{'code': '>>> distance > 0\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ homeworks/01-causality/tests/q52.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q52',
+    'points': 1,
+    'suites': [   {   'cases': [{'code': '>>> india_q2 == "observational study" or india_q2 == "randomized controlled trial" # check for a spelling mistake!\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
