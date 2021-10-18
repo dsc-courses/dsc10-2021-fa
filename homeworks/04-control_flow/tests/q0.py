be2d0670e67b3b4f6bc063f606a53d79472d863d
xd00099 <dxiang@ucsd.edu>
2021-10-18 14:24:20 -0700

    edit hw4 testq_0

--- homeworks/04-control_flow/tests/q0.py
+++ homeworks/04-control_flow/tests/q0.py
@@ -1,6 +1,6 @@
 test = {   'name': 'q0',
     'points': 1,
-    'suites': [   {   'cases': [{'code': '>>> completed_survey == True # Please complete the survey. \nFalse', 'hidden': False, 'locked': False}],
+    'suites': [   {   'cases': [{'code': '>>> completed_survey == True # Please complete the survey. \nTrue', 'hidden': False, 'locked': False}],
                       'scored': True,
                       'setup': '',
                       'teardown': '',
