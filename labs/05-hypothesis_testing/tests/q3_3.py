454ff008d249e186babc092e9fa9fd837623705c
xd00099 <dxiang@ucsd.edu>
2021-10-28 22:08:51 -0700

    added lab5

---
+++ labs/05-hypothesis_testing/tests/q3_3.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q3_3',
+    'points': 1,
+    'suites': [{'cases': [{'code': '>>> 1.5 < np.mean(simulation_results) < 2.5\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ labs/05-hypothesis_testing/tests/q3_4.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q3_4',
+    'points': 1,
+    'suites': [{'cases': [{'code': '>>> 0.01 < phone_p_val < 0.03\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
