c88b096a71cbd3ff14e3d465c6bf93f820ee88cf
xd00099 <dxiang@ucsd.edu>
2021-11-07 14:05:45 -0800

    added lab6

---
+++ labs/06-resampling_and_the_bootstrap/tests/q13.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q13',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> calculate_mean_based_estimate(np.array([1, 2, 3])) is not None\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> int(np.round(calculate_mean_based_estimate(np.array([1, 2, 3])))) == 4\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
