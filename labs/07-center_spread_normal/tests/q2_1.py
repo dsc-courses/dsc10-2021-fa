7cb606adfb8c88dcd2989abefa74a6a886d7b348
xd00099 <dxiang@ucsd.edu>
2021-11-14 09:04:33 -0800

    added lab7

---
+++ labs/07-center_spread_normal/tests/q2_1.py
@@ -0,0 +1,1 @@
+test = {'name': 'q2_1', 'points': 1, 'suites': [{'cases': [{'code': '>>> callable(simulation)\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
