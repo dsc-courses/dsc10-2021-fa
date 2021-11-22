45299051a0d95df3333d526877d7a84cbc03c2d8
xd00099 <dxiang@ucsd.edu>
2021-11-21 21:06:31 -0800

    added lab8

---
+++ labs/08-regression/tests/q1_5.py
@@ -0,0 +1,1 @@
+test = {'name': 'q1_5', 'points': 1, 'suites': [{'cases': [{'code': '>>> correlation_guess == 1\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
